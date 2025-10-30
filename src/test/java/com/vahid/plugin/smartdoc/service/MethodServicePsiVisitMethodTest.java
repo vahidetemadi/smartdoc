bd1239a1cffd2a8e7947d9ac2b282d272d07c3d8
vahidetemadi <vetemadi87@gmail.com>
2025-10-30 20:02:20 +0330

    feat: add license header to all written files

--- src/test/java/com/vahid/plugin/smartdoc/service/MethodServicePsiVisitMethodTest.java
+++ src/test/java/com/vahid/plugin/smartdoc/service/MethodServicePsiVisitMethodTest.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2025 Vahid Etemadi
+ *
+ * Licensed under the Creative Commons Attribution-NoDerivatives 4.0 International License.
+ * You may share this work (copy and redistribute) under the terms of the license,
+ * but you may not remix, transform, or build upon it.
+ * To view a copy of the license, visit https://creativecommons.org/licenses/by-nd/4.0/
+ */
 package com.vahid.plugin.smartdoc.service;
 
 import com.intellij.openapi.application.ApplicationManager;
